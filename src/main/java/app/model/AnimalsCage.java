f18603482349b7a35782fcd48906883a6f039210
alex <borovinskihalex986@gmail.com>
2024-08-01 18:29:05 +0500

    bean hw 1

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,23 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Bean("timer")
+    public Timer getTimer() {
+        Timer timer = new Timer();
+        timer.getTime();
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +25,7 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+
     }
+
 }
